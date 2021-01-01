783bc581bbd5e12a08acd17010694d8c75052d9d
Ben Eich <jgradio64@yahoo.com>
2021-01-01 14:57:57 -0500

    Refactor code. Add some in-code documentation.

--- bot.py
+++ bot.py
@@ -18,6 +18,11 @@ client = discord.Client()
 today = datetime.today()
 
 
+# Build a birthday schema based upon input.
+def create_birthday(user_id, user_name, month, date):
+    return {"_id": user_id, "user": user_name, "month": month, "day": date}
+
+
 # Checks to see if the date giving is within the acceptable range depending on the month.
 def check_date(month, day):
     if month in [12, 10, 8, 7, 5, 3, 1]:
@@ -67,13 +72,23 @@ async def on_message(msg):
             await msg.channel.send('To remove your birthday from the server use "$remove_my_bday"\n'
                                    '\tExample: "$remove_my_bday"\n')
         if msg.content.startswith('$check_bdays'):
+            # Set values to check for in the database
             my_query = {"month": today.month, "day": today.day}
-            print(f'Month: {today.month}\nDate: {today.day}')
-            if collection.count_documents(my_query) == 0:
+            # check the number of birthdays recorded in the database
+            number_of_birthdays = collection.count_documents(my_query)
+            # if there are no birthdays
+            if number_of_birthdays == 0:
                 await msg.channel.send('There are no birthdays today. :weary:')
                 print(collection.find(my_query))
-            if collection.count_documents(my_query) != 0:
+            # If birthdays were found
+            if number_of_birthdays != 0:
+                # Get the birthdays from the database.
                 list_of_birthdays = collection.find(my_query)
+                if number_of_birthdays == 1:
+                    await msg.channel.send(f'There is {number_of_birthdays} birthday today!')
+                else:
+                    await msg.channel.send(f'There are {number_of_birthdays} birthdays today!')
+                # Loop through the birthdays wishing happy birthday to everyone!
                 for birthday in list_of_birthdays:
                     await msg.channel.send("Happy birthday " + birthday['user'] + "!!!")
         if msg.content.startswith('$add_my_bday:'):
@@ -81,14 +96,18 @@ async def on_message(msg):
             # if there is not a preexisting result in the database by that user
             if collection.count_documents(my_query) == 0:
                 date = re.findall("([0-9]+)", msg.content)
+                # get month value, convert from str to int
                 birthday_month = int(date[0])
+                # get date value, convert from str to int
                 birthday_date = int(date[1])
+                # If the month is an acceptable month
                 if check_month(birthday_month):
-                    date_works = check_date(birthday_month, birthday_date)
-                    if date_works:
-                        birthday = {"_id": msg.author.id, "user": msg.author.name, "month": birthday_month,
-                                    "day": birthday_date}
+                    # If the date is an acceptable value based upon the month
+                    if check_date(birthday_month, birthday_date):
+                        # Create Schema for the users birthday
+                        birthday = create_birthday(msg.author.id, msg.author.name, birthday_month, birthday_date)
                         await msg.channel.send(f'Adding your birthday, {msg.author.name}!')
+                        # Insert the data into the Database
                         collection.insert_one(birthday)
                         await msg.channel.send(f'I\'ll send you a birthday wish on {birthday_month}-{birthday_date}!')
                     else:
