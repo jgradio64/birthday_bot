e74fd5f54510203a12daa2cac6388f819c2ce893
Ben Eich <jgradio64@yahoo.com>
2021-01-01 14:42:41 -0500

    Add in date and month validation functions. Change the types of incoming data

--- bot.py
+++ bot.py
@@ -18,6 +18,31 @@ client = discord.Client()
 today = datetime.today()
 
 
+# Checks to see if the date giving is within the acceptable range depending on the month.
+def check_date(month, day):
+    if month in [12, 10, 8, 7, 5, 3, 1]:
+        if day in range(1, 31):
+            return True
+        else:
+            return False
+    elif month in [4, 6, 9, 11]:
+        if day in range(1, 30):
+            return True
+        else:
+            return False
+    elif month == 2:
+        if day in range(1, 28):
+            return True
+        else:
+            return False
+
+
+# Checks to see if the month is within the range of possible selections
+def check_month(month):
+    month_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    return month in month_range
+
+
 # On connection to discord.
 @client.event
 async def on_ready():
@@ -35,13 +60,15 @@ async def on_message(msg):
         if msg.content.startswith('I love you'):
             await msg.channel.send('I\'m not ready for this level of commitment. :flushed:')
         if msg.content.startswith('$how?'):
-            await msg.channel.send('To check if any one has a birthday today just type "$check_bdays"\n\n\n')
+            await msg.channel.send('To check if any one has a birthday today just type "$check_bdays"\n')
             await msg.channel.send('To add your birthday use "$add_my_bday: Month-Day".\n'
                                    'Use digits for the month and day.\n'
-                                   'Example: "$add_my_bday: 11-04"\n\n\n')
-            await msg.channel.send('To remove your birthday from the server use "$remove_my_bday"\n\n')
+                                   '\tExample: "$add_my_bday: 11-4"\n')
+            await msg.channel.send('To remove your birthday from the server use "$remove_my_bday"\n'
+                                   '\tExample: "$remove_my_bday"\n')
         if msg.content.startswith('$check_bdays'):
-            my_query = {"month": f'{today.month}', "day": f'{today.day}'}
+            my_query = {"month": today.month, "day": today.day}
+            print(f'Month: {today.month}\nDate: {today.day}')
             if collection.count_documents(my_query) == 0:
                 await msg.channel.send('There are no birthdays today. :weary:')
                 print(collection.find(my_query))
@@ -53,9 +80,9 @@ async def on_message(msg):
             my_query = {"_id": msg.author.id}
             # if there is not a preexisting result in the database by that user
             if collection.count_documents(my_query) == 0:
-                date = re.findall("([0-9]{2})", msg.content)
-                birthday_month = date[0]
-                birthday_date = date[1]
+                date = re.findall("([0-9]+)", msg.content)
+                birthday_month = int(date[0])
+                birthday_date = int(date[1])
                 if check_month(birthday_month):
                     date_works = check_date(birthday_month, birthday_date)
                     if date_works:
@@ -83,25 +110,3 @@ async def on_message(msg):
 
 
 client.run(os.environ.get("TOKEN"))
-
-
-def check_date(month, day):
-    if int(month) in [12, 10, 8, 7, 5, 3, 1]:
-        if int(day) in range(1, 31):
-            return True
-        else:
-            return False
-    elif int(month) in [4, 6, 9, 11]:
-        if int(day) in range(1, 30):
-            return True
-        else:
-            return False
-    elif int(month) == 2:
-        if int(day) in range(1, 28):
-            return True
-        else:
-            return False
-
-
-def check_month(month):
-    return int(month) in range(1, 12)
