7c727ff7a0084312941d362802b4cadb660f0ce1
jgradio64 <47864214+jgradio64@users.noreply.github.com>
2021-08-21 15:32:15 -0400

    Update comments

--- my_imports.py
+++ my_imports.py
@@ -5,13 +5,14 @@ from datetime import datetime
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+# Load the environmental variables
 load_dotenv()
 
-# connecting to the MongoDB server, then the database, then the collection.
+# connecting to the MongoDB server, then the cluster, then the collection.
 cluster = MongoClient(f'{os.environ.get("CONNECTION_URL")}')
 db = cluster["user_data"]
 collection = db["user_birthdays"]
 
-# Initializing instances of the used objects. Also calling the function to load from .env files.
+# Initializing instances of the used objects.
 client = discord.Client()
 today = datetime.today()
